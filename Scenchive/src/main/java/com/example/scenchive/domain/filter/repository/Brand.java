50e3bc87815f7c68089954fc49a2d6f291e6bd33
Choi YeJin <amy350500@gmail.com>
2023-05-25 00:24:14 +0900

    키워드 필터링 2차 구현 (브랜드명, 키워드 리스트 추가)

---
+++ Scenchive/src/main/java/com/example/scenchive/domain/filter/repository/Brand.java
@@ -0,0 +1,20 @@
+package com.example.scenchive.domain.filter.repository;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "brand" )
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "brand_name")
+    private String brandName;
+}
