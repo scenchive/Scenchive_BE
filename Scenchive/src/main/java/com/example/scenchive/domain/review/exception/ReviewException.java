6d9b021d20a71e0ce42f95c498a8f08a9aad0273
Choi YeJin <amy350500@gmail.com>
2023-05-27 01:08:22 +0900

    review exception 패키지 삭제

--- Scenchive/src/main/java/com/example/scenchive/domain/review/exception/ReviewException.java
+++
@@ -1,7 +0,0 @@
-package com.example.scenchive.domain.review.exception;
-
-public class ReviewException extends RuntimeException {
-    public ReviewException(String message) {
-        super(message);
-    }
-}
