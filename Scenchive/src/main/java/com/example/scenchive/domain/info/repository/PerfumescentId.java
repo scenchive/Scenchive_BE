82d500c70748b76e9ab4c566022f2549acb1dd10
jeongin <enen9032@naver.com>
2023-06-16 00:51:53 +0900

    개별 향수 노트 정보 조회 (2차 구현)

---
+++ Scenchive/src/main/java/com/example/scenchive/domain/info/repository/PerfumescentId.java
@@ -0,0 +1,13 @@
+package com.example.scenchive.domain.info.repository;
+
+import com.example.scenchive.domain.filter.repository.Perfume;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class PerfumescentId {
+    private Perfume perfume;
+    private Perfumenote perfumenote;
+    private String scent;
+}
