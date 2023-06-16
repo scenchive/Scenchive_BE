e9c163d19dad0e08550c0921e89a32c5864395e5
Choi YeJin <amy350500@gmail.com>
2023-06-16 22:56:29 +0900

    판매처(mallName) 추가

--- Scenchive/src/main/java/com/example/scenchive/domain/shopping/dto/ItemDto.java
+++ Scenchive/src/main/java/com/example/scenchive/domain/shopping/dto/ItemDto.java
@@ -11,11 +11,14 @@ public class ItemDto implements Comparable<ItemDto> {
     private String image;
     private int lprice;
 
+    private String mallName;
+
     public ItemDto(JSONObject itemJson) {
         this.title = itemJson.getString("title");
         this.link = itemJson.getString("link");
         this.image = itemJson.getString("image");
         this.lprice = itemJson.getInt("lprice");
+        this.mallName = itemJson.getString("mallName");
     }
 
     @Override
