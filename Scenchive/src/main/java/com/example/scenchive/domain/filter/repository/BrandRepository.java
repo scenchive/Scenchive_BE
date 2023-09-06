32f4abe47d16f609e5d8f760f41744949920e505
Choi YeJin <amy350500@gmail.com>
2023-09-06 20:34:17 +0900

    feat: 브랜드 한글 검색 기능 구현

--- Scenchive/src/main/java/com/example/scenchive/domain/filter/repository/BrandRepository.java
+++ Scenchive/src/main/java/com/example/scenchive/domain/filter/repository/BrandRepository.java
@@ -3,6 +3,7 @@ package com.example.scenchive.domain.filter.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +14,8 @@ public interface BrandRepository extends JpaRepository<Brand, Long> {
     List<Brand> findByBrandName(String brandName);
 
     List<Brand> findByBrandNameContainingIgnoreCase(String brandname);
+
+    @Query("SELECT b FROM Brand b WHERE LOWER(b.brandName_kr) LIKE LOWER(CONCAT('%', :brandname, '%'))")
+    List<Brand> findByBrandNameKrContainingIgnoreCase(@Param("brandname") String brandname);
 
 }
