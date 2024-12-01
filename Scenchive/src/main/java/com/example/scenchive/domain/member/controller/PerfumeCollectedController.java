75b86a930a79f23ba53c527f22552f1e3e9e93b5
HyominAn0401 <slhm7547@naver.com>
2024-12-02 00:55:09 +0900

    Fix : 예외 상황 구현

--- Scenchive/src/main/java/com/example/scenchive/domain/member/controller/PerfumeCollectedController.java
+++ Scenchive/src/main/java/com/example/scenchive/domain/member/controller/PerfumeCollectedController.java
@@ -1,5 +1,6 @@
 package com.example.scenchive.domain.member.controller;
 
+import com.amazonaws.Response;
 import com.example.scenchive.domain.filter.dto.PerfumeCollectedRequestDto;
 import com.example.scenchive.domain.filter.dto.PerfumeCollectedResponseDto;
 import com.example.scenchive.domain.filter.repository.BrandRepository;
@@ -7,6 +8,7 @@ import com.example.scenchive.domain.member.repository.MemberRepository;
 import com.example.scenchive.domain.member.service.MemberService;
 import com.example.scenchive.domain.member.service.PerfumeCollectedService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,22 +20,36 @@ public class PerfumeCollectedController {
 
     private final PerfumeCollectedService perfumeCollectedService;
 
+    // 사용자 보유 향수 추가
     @PostMapping("/user/perfume-collect")
     public ResponseEntity<String> addPerfumeToCollection(@RequestBody PerfumeCollectedRequestDto perfumeCollectedRequestDto){
-        PerfumeCollectedResponseDto perfumeCollectedResponseDto = perfumeCollectedService.addPerfumeToCollection(perfumeCollectedRequestDto);
-        return ResponseEntity.ok("향수가 보유 목록에 추가되었습니다.");
+        try{
+            PerfumeCollectedResponseDto perfumeCollectedResponseDto = perfumeCollectedService.addPerfumeToCollection(perfumeCollectedRequestDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body("향수가 보유 목록에 추가되었습니다.");
+        } catch(RuntimeException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
     @GetMapping("/user/perfume-collect")
-    public ResponseEntity<List<PerfumeCollectedResponseDto>> getCollectedPerfumes(){
+    public ResponseEntity<?> getCollectedPerfumes(){
         List<PerfumeCollectedResponseDto> responseDtos = perfumeCollectedService.getCollectedPerfumes();
+
+        if(responseDtos.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("보유한 향수가 없습니다.");
+        }
+
         return ResponseEntity.ok(responseDtos);
     }
 
     @DeleteMapping("/user/perfume-collect/{perfumeId}")
     public ResponseEntity<String> removePerfumeFromCollection(@PathVariable Long perfumeId){
-        perfumeCollectedService.removePerfumeFromCollection(perfumeId);
-        return ResponseEntity.ok("향수가 보유 목록에서 삭제되었습니다.");
+        try{
+            perfumeCollectedService.removePerfumeFromCollection(perfumeId);
+            return ResponseEntity.ok("향수가 보유 목록에서 삭제되었습니다.");
+        } catch(RuntimeException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
 }
--- Scenchive/src/main/java/com/example/scenchive/domain/member/service/PerfumeCollectedService.java
+++ Scenchive/src/main/java/com/example/scenchive/domain/member/service/PerfumeCollectedService.java
@@ -8,7 +8,10 @@ import com.example.scenchive.domain.filter.utils.BrandMapper;
 import com.example.scenchive.domain.member.repository.Member;
 import com.example.scenchive.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,6 +24,7 @@ public class PerfumeCollectedService {
     private final BrandRepository brandRepository;
     private final MemberRepository memberRepository;
     private final MemberService memberService;
+    private final PerfumeRepository perfumeRepository;
 
     // 보유 향수 추가
     public PerfumeCollectedResponseDto addPerfumeToCollection(PerfumeCollectedRequestDto perfumeCollectedRequestDto){
@@ -28,22 +32,23 @@ public class PerfumeCollectedService {
         Member currentMember = getCurrentMember();
 
         // 중복 확인
-        if(perfumeCollectedRepository.findByMemberIdAndPerfumeId(currentMember.getId(), perfumeCollectedRequestDto.getPerfumeId()).isPresent()){
+        if (perfumeCollectedRepository.findByMemberIdAndPerfumeId(getCurrentMember().getId(), perfumeCollectedRequestDto.getPerfumeId()).isPresent()) {
             throw new RuntimeException("이미 보유하고 있는 향수입니다.");
         }
 
-        Perfume perfume = new Perfume();
-        perfume.setId(perfumeCollectedRequestDto.getPerfumeId());
+        // perfumeId 유효 확인
+        Perfume perfume = perfumeRepository.findById(perfumeCollectedRequestDto.getPerfumeId())
+                .orElseThrow(() -> new RuntimeException("해당 향수가 존재하지 않습니다."));
 
+        // 보유 향수 등록
         PerfumeCollected perfumeCollected = new PerfumeCollected();
         perfumeCollected.setMember(currentMember);
         perfumeCollected.setPerfume(perfume);
 
         PerfumeCollected savedPerfumeCollected = perfumeCollectedRepository.save(perfumeCollected);
 
-        // brand
         BrandDto brandDto = BrandMapper.toBrandDto(
-                brandRepository.findById(perfumeCollectedRequestDto.getPerfumeId()).orElseThrow(() -> new RuntimeException("브랜드 정보를 찾을 수 없습니다."))
+                brandRepository.findById(perfume.getBrandId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "브랜드 정보를 찾을 수 없습니다."))
         );
 
         return new PerfumeCollectedResponseDto(
@@ -56,19 +61,21 @@ public class PerfumeCollectedService {
     }
 
     // 보유 향수 삭제
+    @Transactional
     public void removePerfumeFromCollection(Long perfumeId){
         Member currentMember = getCurrentMember();
 
         // 보유 향수 확인하기
         PerfumeCollected perfumeCollected = perfumeCollectedRepository
                 .findByMemberIdAndPerfumeId(currentMember.getId(), perfumeId)
-                .orElseThrow(() -> new RuntimeException("해당 향수를 보유 목록에서 찾을 수 없습니다."));
+                .orElseThrow(() -> new RuntimeException("해당 향수가 보유 목록에 없습니다."));
 
         // 삭제
         perfumeCollectedRepository.delete(perfumeCollected);
     }
 
     // 보유 향수 조회
+    @Transactional(readOnly = true)
     public List<PerfumeCollectedResponseDto> getCollectedPerfumes(){
         Member currentMember = getCurrentMember();
 
@@ -90,10 +97,10 @@ public class PerfumeCollectedService {
                 }).collect(Collectors.toList());
     }
 
-    private Member getCurrentMember(){
+    private Member getCurrentMember() {
         return memberRepository.findByEmail(
                 memberService.getMyUserWithAuthorities().getEmail()
-        ).orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+        ).orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "사용자 정보를 찾을 수 없습니다."));
     }
 
 }
