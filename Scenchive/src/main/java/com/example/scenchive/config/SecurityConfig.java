9faf071044adac5f92fa22e4ec4d5367a94f2098
naahnoey <naahyin20@sookmyung.ac.kr>
2024-12-23 18:22:26 +0900

    Feat: 로그인 없이 인기순 향수 볼 수 있도록 수정

--- Scenchive/src/main/java/com/example/scenchive/config/SecurityConfig.java
+++ Scenchive/src/main/java/com/example/scenchive/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
                 .requestMatchers("/signup", "/login", "/survey", "/member/**",
                         "/randomperfume", "/search", "/brandperfume", "/perfumes/**",
                         "/reviews/**", "/perfumerating/**", "/product/search", "/notesinfo/**",
-                        "/fullinfo/**", "/email/**", "/main/**").permitAll()
+                        "/fullinfo/**", "/email/**", "/main/**", "/popular").permitAll()
                 .requestMatchers("/master/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
 
