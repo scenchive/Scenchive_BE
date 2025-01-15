51eff1f8873a5f4d5502792d58575500195622a1
naahnoey <naahyin20@sookmyung.ac.kr>
2025-01-15 11:50:56 +0900

    Feat: 테이블에서 scent, scentKr 정보만 가져오기

--- Scenchive/src/main/java/com/example/scenchive/domain/info/repository/PerfumescentRepository.java
+++ Scenchive/src/main/java/com/example/scenchive/domain/info/repository/PerfumescentRepository.java
@@ -1,6 +1,7 @@
 package com.example.scenchive.domain.info.repository;
 
 import com.example.scenchive.domain.filter.repository.Perfume;
+import com.example.scenchive.domain.info.dto.ScentAndScentKr;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +11,9 @@ import java.util.List;
 
 @Repository
 public interface PerfumescentRepository extends JpaRepository<Perfumescent, Long> {
+    // 노트 정보 가져오기
+    List<ScentAndScentKr> findAllBy();
+
     // 전달 받은 향수 id와 노트 id에 해당하는 Perfumescent의 데이터 row 접근
     List<Perfumescent> findByPerfumeAndPerfumenote(Perfume perfume, Perfumenote perfumenote);
 
